f99ef816764f1a235f5368999f2e2953f0b63343
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-05-08 16:31:09 +0100

    First home version

---
+++ home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+{
+    home.username = "alexis";
+    home.homeDirectory = "/home/alexis";
+    /* Should not change */
+    home.stateVersion = "24.11";
+
+    home.packages = with pkgs;
+    [
+        nerd-fonts.jetbrains-mono
+        nerd-fonts.fantasque-sans-mono
+    ];
+    fonts.fontconfig.enable = true;
+
+    home.file = {
+    };
+
+    home.sessionVariables = {
+        EDITOR = "vim";
+    };
+
+    programs.home-manager.enable = true;
+    programs.fastfetch.enable = true;
+}
