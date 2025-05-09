0ff2dcd631ec6ea23693a4b38144727d64bd06ef
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-05-09 16:11:25 +0100

    Add lazygit and kitty to home-manager

--- home.nix
+++ home.nix
@@ -22,6 +22,7 @@
     programs = {
         home-manager.enable = true;
         fastfetch.enable = true;
+        lazygit.enable = true;
         zoxide = {
             enable = true;
             enableZshIntegration = true;
@@ -31,6 +32,41 @@
             enable = true;
             enableZshIntegration = true;
             icons = "auto";
+        };
+        kitty = {
+            enable = true;
+            font = {
+                name = "FantasqueSansM Nerd Font Mono";
+                size = 14.0;
+            };
+            themeFile = "Galaxy";
+            enableGitIntegration = true;
+            /* Will be changed later. */
+            /* Need to configure zsh before with home-manager. */
+            shellIntegration.mode = null;
+            /*
+            shellIntegration = {
+                enableZshIntegration = true;
+                mode = "enabled";
+            };
+            */
+            settings = {
+                enable_audio_bell = false;
+
+                background_opacity = 0.9;
+                dynamic_background_opacity = 1;
+                confirm_os_window_close = 0;
+
+                cursor_trail = 1;
+                scrollback_lines = 1500;
+
+                window_padding_width = 3;
+
+                selection_foreground = "none";
+                selection_background = "none";
+
+                background = "#000000";
+            };
         };
     };
 }
