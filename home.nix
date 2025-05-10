eaf4180ecb678015ffaaafebd3053e9685e1d168
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-05-10 11:36:36 +0100

    Add git config

--- home.nix
+++ home.nix
@@ -68,5 +68,16 @@
                 background = "#000000";
             };
         };
+        git = {
+            enable = true;
+            aliases = {
+                /* Inspired by a command on stackoverflow */
+                adog = "log --all --decorate --oneline --graph";
+            };
+            lfs.enable = true;
+            userName = "Alexis Fouquet";
+            /* Public email for github */
+            userEmail = "144385093+Alexis-Fouquet@users.noreply.github.com";
+        };
     };
 }
