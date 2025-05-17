46b644c33777d95e8e27b68a28c8bf7be9f726c1
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-05-17 22:16:39 +0100

    Split home config for zsh

---
+++ zsh.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, lib, ... }:
+{
+    programs.zsh = {
+        enable = true;
+        autocd = true;
+        zprof.enable = false;
+        shellAliases = {
+            vim = "nvim";
+            ":q" = "exit";
+            cat = "bat";
+            icat = "kitten icat";
+        };
+        completionInit = "autoload -Uz compinit && compinit -C";
+        antidote = {
+            enable = true;
+            plugins = [
+                "romkatv/powerlevel10k"
+                "getantidote/use-omz"
+                "ohmyzsh/ohmyzsh path:lib"
+                "ohmyzsh/ohmyzsh path:plugins/git"
+                "ohmyzsh/ohmyzsh path:plugins/colored-man-pages"
+                "zsh-users/zsh-autosuggestions"
+                "zsh-users/zsh-syntax-highlighting kind:defer"
+                "MichaelAquilina/zsh-you-should-use"
+            ];
+        };
+        initContent =
+            let early = lib.mkOrder 500
+            ''
+alias datenow="date +\"%s,%N\""
+
+# datenow
+INSTANT_PROMPT="''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+if [[ -r $INSTANT_PROMPT ]]; then
+    source $INSTANT_PROMPT
+fi
+
+if [[ ! $1 ]] && [[ -f ~/.p10k.zsh ]]; then
+    source ~/.p10k.zsh
+    p10k_applied=true
+fi
+
+# datenow
+# echo "Start 1"
+            '';
+            before = lib.mkOrder 550
+            ''
+# datenow
+# echo "Start 2"
+            '';
+            after = lib.mkOrder 1000
+            ''
+# datenow
+if [[ ! $1 ]] && [[ -f ~/.p10k.zsh ]]; then
+    source ~/.p10k.zsh
+    p10k_applied=true
+fi
+            '';
+            in lib.mkMerge [early before after];
+    };
+}
