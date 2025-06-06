57dbf3375cb7c62e3e62c3ccfcfd649f8167a059
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-06-06 13:22:44 +0200

    Test a .gitignore

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+    /* Made using the template on home-manager repository */
+    description = "Home Manager configuration of Alexis Fouquet";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+        home-manager = {
+            url = "github:nix-community/home-manager/release-25.05";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+        homeConfigurations.alexis = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [ ./home.nix ];
+        };
+    };
+}
