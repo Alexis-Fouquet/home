aeede6f437876230a5e2bfeb69b78444e1b89b7b
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-07-02 22:33:53 +0200

    Minimal config

--- nixvim.nix
+++ nixvim.nix
@@ -12,19 +12,46 @@
         };
 
         plugins = {
+            which-key.enable = true;
             telescope = {
                 enable = true;
-                autoLoad = true;
+                extensions.fzf-native.enable = true;
             };
-
-            web-devicons.enable = true;
 
             wakatime.enable = true;
             neo-tree.enable = true;
 
+            web-devicons.enable = true;
             notify.enable = true;
+            smear-cursor.enable = true;
+            lualine.enable = true;
+        };
+
+        opts = {
+            autoindent = true;
+            smartindent = true;
+            expandtab = true;
+            shiftwidth = 4;
+            tabstop = 4;
+
+            colorcolumn = "80";
+            signcolumn = "yes";
+            cursorline = true;
+            ruler = true;
+            relativenumber = true;
+            number = true;
+
+            ignorecase = true;
+            smartcase = true;
+
+            conceallevel = 2;
+
+            list = true;
+            listchars = {
+                tab = ">~";
+                trail = "~";
+            };
         };
     };
-
 }
 
