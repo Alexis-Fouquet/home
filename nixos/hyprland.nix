ed72fd814ff94d126628b4cccb7f8da41060590a
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-08-01 11:32:59 +0200

    Add some keys and config

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -1,8 +1,15 @@
 { pkgs, ... }:
-{
+let mod = "SUPER";
+alt = "SHIFT";
+alt2 = "CTRL";
+browser = "firefox";
+terminal = "kitty";
+drun = "wofi --show drun";
+in {
     home.packages = with pkgs;
     [
     wofi
+    nautilus
     ];
 
     services.hyprpaper.enable = true;
@@ -14,19 +21,21 @@
     };
 
     wayland.windowManager.hyprland.settings = {
-        "$mod" = "SUPER";
-        "$browser" = "firefox";
-        "$terminal" = "kitty";
         "$menu" = "wofi --show drun";
 
         bind = [
-        "$mod, B, exec, $browser"
-        "$mod, D, exec, $menu"
-        "$mod, T, exec, $terminal"
-        "$mod, L, exec, hyprlock"
-        "$mod, Q, killactive,"
-        "$mod, E, exit,"
-        "$mod, A, hyprexpo:expo, toggle"
+        "${mod}, B, exec, ${browser}"
+        "${mod}, D, exec, ${drun}"
+        "${mod}, T, exec, ${terminal}"
+        "${mod}, L, exec, hyprlock"
+
+        "${mod}, Q, killactive,"
+        "${mod} ${alt}, Q, killactive,"
+        "${mod}, SPACE, togglefloating,"
+        "${mod}, F, fullscreen,"
+
+        "${mod}, E, exit,"
+        "${mod}, A, hyprexpo:expo, toggle"
         ]
         ++ (
                 /* Half from the Hyprland documentation about Nix */
@@ -36,15 +45,30 @@
                         key = toString i;
                         ws_name = toString ws_id;
                         in [
-                        "$mod, code:1${key}, workspace, ${ws_name}"
-                        "$mod SHIFT, code:1${key}, movetoworkspace, ${ws_name}"
+                        "${mod}, code:1${key}, workspace, ${ws_name}"
+                        "${mod} ${alt}, code:1${key}, movetoworkspace, ${ws_name}"
+                        "${mod} ${alt2}, code:1${key}, movetoworkspacesilent, ${ws_name}"
                         ]
                         )
                     9)
            );
 
+        binds = {
+            drag_threshold = 5;
+        };
+
+        bindm = [
+        "${mod}, mouse:272, movewindow"
+        "${mod}, mouse:273, resizewindow"
+        "${mod}, Control_L, movewindow"
+        ];
+
+        bindc = [
+        "${mod}, mouse:272, togglefloating"
+        ];
+
         exec-once = [
-            "$terminal --hold zsh -c \"cbonsai -lim \"Hello!\"\""
+            "${terminal} --hold zsh -c \"cbonsai -lim \"Hello!\"\""
         ];
 
         input = {
@@ -55,6 +79,11 @@
                 disable_while_typing = true;
             };
         };
+
+        monitor = [
+            "eDP-1,    preferred,  auto-right, 1"
+            "HDMI-A-1, preferrred, auto-left,  1"
+        ];
     };
 
     wayland.windowManager.hyprland.plugins = with pkgs.hyprlandPlugins;
