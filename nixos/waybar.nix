54651c48de9c43ee6f5cb2d4d4a5459b217c448d
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-08-01 17:15:07 +0200

    Start exploring waybar configuration

--- nixos/waybar.nix
+++ nixos/waybar.nix
@@ -4,4 +4,31 @@
         enable = true;
         systemd.enable = true;
     };
+
+    programs.waybar.settings.mainBar = {
+        height = 35;
+        spacing = 5;
+
+        modules-left = [
+        "hyprland/workspaces"
+        "hyprland/submap"
+        ];
+
+        modules-center = [
+        "idle_inhibitor"
+        "hyprland/window"
+        ];
+
+        modules-right = [
+        "mdp"
+        "pulseaudio"
+        "network"
+        "cpu"
+        "memory"
+        "temperature"
+        "battery"
+        "clock"
+        "tray"
+        ];
+    };
 }
