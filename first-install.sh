5ee42c1a6c56ad07f7049730a5599c6ac36a5b04
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-09-29 11:40:19 +0200

    fix: cd to old pwd

--- first-install.sh
+++ first-install.sh
@@ -1,5 +1,6 @@
 
 echo "Installing configuration"
-nix-shell -p home-manager nix-output-monitor --run "pwd; make reload-home"
-# nix run home-manager/release-25.05 -- switch --impure
+FOLDER=$(pwd)
+nix-shell -p home-manager nix-output-monitor \
+    --run "cd $FOLDER; pwd; make reload-home"
 
