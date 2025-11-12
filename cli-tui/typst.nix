c6b7a1fed0f85d9bff9e98eb05262780b88c1f94
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-11-12 19:26:28 +0100

    add(typst): first config for typst

--- cli-tui/typst.nix
+++ cli-tui/typst.nix
@@ -1,3 +1,24 @@
-{ ... }:
 {
+  grammar,
+  pkgs,
+  ...
+}:
+{
+  home.packages = with pkgs; [
+    typst
+  ];
+
+  programs.nixvim = {
+    plugins = {
+      typst-preview.enable = true;
+      treesitter = {
+        grammarPackages = with grammar; [
+          typst
+        ];
+      };
+    };
+    lsp.servers = {
+      tinymist.enable = true;
+    };
+  };
 }
