070120bd51474a799d819b0a47dd6a2b33f2e536
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-11-23 17:57:15 +0100

    update: update to unstable while waiting for 25.11

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
   description = "Home Manager and NixOS configuration of Alexis Fouquet";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-25.05";
+      url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.6.0";
