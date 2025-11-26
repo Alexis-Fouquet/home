fe71e063ceb29ac45e1b1033208f670df95fba5b
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-11-26 22:48:36 +0100

    fix(25.11): add home manager, building

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
