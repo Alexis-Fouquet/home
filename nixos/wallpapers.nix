8db522910acc6403370372845223b0817dbac83c
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2026-07-28 21:26:32 +0200

    add(dms): some wallpapers

--- nixos/wallpapers.nix
+++ nixos/wallpapers.nix
@@ -3,6 +3,8 @@ let
   # A wallpaper from the collection wallpkgs
   # See https://github.com/NotAShelf/wallpkgs for License
   wallpaper = wallpkgs.wallpapers.tokyo-night.tokyo_night-03.path;
+  wallpaper2 = wallpkgs.wallpapers.tokyo-night.tokyo_night-02.path;
+  wallpaper1 = wallpkgs.wallpapers.tokyo-night.tokyo_night-01.path;
 in
 {
   services.hyprpaper = {
@@ -17,4 +19,8 @@ in
       ];
     };
   };
+
+  home.file.".wallpapers/wall.png".source = wallpaper;
+  home.file.".wallpapers/wall2.png".source = wallpaper2;
+  home.file.".wallpapers/wall1.png".source = wallpaper1;
 }
