da22ce9a1ab9d65609100aaa8fff4e7b8d4f0a42
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2026-09-25 19:09:24 +0200

    add(wallpapers): more wallpapers

--- nixos/wallpapers.nix
+++ nixos/wallpapers.nix
@@ -3,6 +3,15 @@ let
   # A wallpaper from the collection wallpkgs
   # See https://github.com/NotAShelf/wallpkgs for License
   wallpaper = wallpkgs.wallpapers.tokyo-night.tokyo_night-03.path;
+  specific = {
+    cat-4 = wallpkgs.wallpapers.catppuccin.catppuccin-04;
+    cat-1 = wallpkgs.wallpapers.catppuccin.catppuccin-01;
+    cat-3d = wallpkgs.wallpapers.catppuccin.catppuccin-3d-model;
+    cat-at = wallpkgs.wallpapers.catppuccin.catppuccin-abandoned-trainstation;
+    cat-as = wallpkgs.wallpapers.catppuccin.catppuccin-abstract-swirls;
+    cat-astro = wallpkgs.wallpapers.catppuccin.catppuccin-astronaut;
+    cat-atl = wallpkgs.wallpapers.catppuccin.catppuccin-atlantis;
+  };
 in
 {
   services.hyprpaper = {
@@ -27,6 +36,7 @@ in
         lib.attrsets.mergeAttrsList [
           wallpkgs.wallpapers.tokyo-night
           wallpkgs.wallpapers.space
+          specific
         ]
       );
 }
